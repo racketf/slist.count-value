1a466edad4a123f1c76edd6d411ac272260b5a8e
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 17:20:23 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,21 +1,21 @@
 #lang racket
 
-; (count-occurrences.sexp s sexp): S X SE -> N
+; (slist.count-value-sexp s sexp): S X SE -> N
 ; returns number of occurrences of s in sexp
-(define count-occurrences.sexp
+(define slist.count-value-sexp
   (lambda (s sexp)
     (if (or (symbol? sexp) (number? sexp))
        (if (eq? sexp s)
            1
            0)
-       (count-occurrences s sexp))))
+       (slist.count-value s sexp))))
 
-; (count-occurrences s slist): S X SL -> N
+; (slist.count-value s slist): S X SL -> N
 ; returns occurrences of s in slist
 ; : size(slist)=0 -> 0
-; : else          -> (count-occurrences.sexp s lst[0]) + (count-occurrences s lst[1..end])
-(define count-occurrences
+; : else          -> (slist.count-value-sexp s lst[0]) + (slist.count-value s lst[1..end])
+(define slist.count-value
   (lambda (s slist)
     (if (null? slist)
         0
-        (+ (count-occurrences.sexp s (car slist)) (count-occurrences s (cdr slist))))))
+        (+ (slist.count-value-sexp s (car slist)) (slist.count-value s (cdr slist))))))
